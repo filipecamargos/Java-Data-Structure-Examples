07e435e39feb3ef24a648a50f2e14d746695628c
filipecamargos <filipe.atrabalho@gmail.com>
2020-12-02 22:26:02 -0700

    Queue

---
+++ Queues/src/ferreira/Main.java
@@ -0,0 +1,16 @@
+package ferreira;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee juca = new Employee("Jane", "Jones", 123);
+        Employee bulla = new Employee("John", "Doe", 4567);
+        Employee maker = new Employee("Mary", "Smith", 22);
+        Employee walker = new Employee("Mike", "Wilson", 3245);
+
+        ArrayQueue queue = new ArrayQueue(10);
+
+
+    }
+}
